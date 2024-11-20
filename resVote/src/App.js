3c7bddbb4308dd4bcf23665ce8d3d23531b020f4
Tyler-Culp <tculp@ucsd.edu>
2024-11-20 13:44:31 -0800

    Run with 'npm start'

--- resVote/src/App.js
+++ resVote/src/App.js
@@ -1,50 +1,78 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './pages/About';
+import Layout from './pages/Navbar';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Login from './components/Login';
-import TransactionForm from './components/TransactionForm';
-import Loader from './components/Loader';
+import TransactionForm from './provided_components/TransactionForm';
+import Loader from './provided_components/Loader';
+import Signup from './pages/Signup';
 
-function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [token, setToken] = useState(null);
-  const [isLoadingAfterLogin, setIsLoadingAfterLogin] = useState(false);
+// function App() {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+//   const [token, setToken] = useState(null);
+//   const [isLoadingAfterLogin, setIsLoadingAfterLogin] = useState(false);
 
-  useEffect(() => {
-    const storedToken = sessionStorage.getItem('token');
-    if (storedToken) {
-      setToken(storedToken);
-      setIsAuthenticated(true);
-    }
-  }, []);
+//   useEffect(() => {
+//     const storedToken = sessionStorage.getItem('token');
+//     if (storedToken) {
+//       setToken(storedToken);
+//       setIsAuthenticated(true);
+//     }
+//   }, []);
 
-  const handleLogin = (authToken) => {
-    setIsLoadingAfterLogin(true);
-    setToken(authToken);
-    sessionStorage.setItem('token', authToken);
+//   const handleLogin = (authToken) => {
+//     setIsLoadingAfterLogin(true);
+//     setToken(authToken);
+//     sessionStorage.setItem('token', authToken);
 
-    setTimeout(() => {
-      setIsAuthenticated(true);
-      setIsLoadingAfterLogin(false);
-    }, 2000);
-  };
+//     setTimeout(() => {
+//       setIsAuthenticated(true);
+//       setIsLoadingAfterLogin(false);
+//     }, 2000);
+//   };
 
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    setToken(null);
-    sessionStorage.removeItem('token');
-  };
+//   const handleLogout = () => {
+//     setIsAuthenticated(false);
+//     setToken(null);
+//     sessionStorage.removeItem('token');
+//   };
 
-  return (
-    <div className="App">
-      {isLoadingAfterLogin && <Loader />}
-      {!isLoadingAfterLogin && isAuthenticated ? (
-        <TransactionForm onLogout={handleLogout} token={token} />
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       {isLoadingAfterLogin && <Loader />}
+//       {!isLoadingAfterLogin && isAuthenticated ? (
+//         <TransactionForm onLogout={handleLogout} token={token} />
+//       ) : (
+//         <Login onLogin={handleLogin} />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App(){
+  return(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+
+            <Route index element={<Home />} />
+
+            <Route path="about" element={<About />} />
+
+            <Route path="login" element={<Login />} />
+
+            <Route path="signup" element={<Signup />} />
+
+            <Route path="*" element={<NoPage />} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  )
 }
-
-export default App;
