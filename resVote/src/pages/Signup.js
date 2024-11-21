00dc0ae0f361f97df57162667e01c5fbc28ff900
Tyler-Culp <tculp@ucsd.edu>
2024-11-20 23:04:04 -0800

    Started work on Signup front end

--- resVote/src/pages/Signup.js
+++ resVote/src/pages/Signup.js
@@ -1,7 +1,75 @@
+import { Button, Form, Input } from "antd";
+import bcrypt from "bcryptjs";
+
+async function submitValues(event) {
+    const username = event.username;
+    const password = event.password
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    user = {username : hashedPassword};
+    
+
+}
+
+function failedSubmission() {
+    return false;
+}
+
 export default function Signup() {
     return (
         <>
-            <h1>Sign Up</h1>
+            <h1>Login Page</h1>
+            <Form
+                name="basic"
+                labelCol={{
+                span: 8,
+                }}
+                wrapperCol={{
+                span: 16,
+                }}
+                style={{
+                maxWidth: 600,
+                }}
+                initialValues={{
+                remember: true,
+                }}
+                onFinish={submitValues}
+                onFinishFailed={failedSubmission}
+                autoComplete="off"
+            >
+                <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Please input your username!',
+                    },
+                ]}
+                >
+                <Input />
+                </Form.Item>
+
+                <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Please input your password!',
+                    },
+                ]}
+                >
+                <Input.Password />
+                </Form.Item>
+
+                <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                </Form.Item>
+            </Form>
         </>
     )
 }
