558a9a45630da33ea81c57c969a3d0cdeca50b8d
Ethan He <yfhe@ucdavis.edu>
2024-11-30 23:03:11 +0000

    fix: only use ascii for voter_id

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from resdb_orm import ResDBORM
 import fire
 import json
+from hypothesis import given
+from hypothesis.strategies import lists
 
-from src.datatype import Vote
+from src.datatype import Vote, vote_gen, vote_list_gen
 from src.vote_server import VoteServer
+
+
+def generate_votes() -> list[Vote]:
+    return vote_list_gen.example()
 
 
 def main(config_path: str = "config.yaml", server_log_path: str | None = None) -> None:
     server = VoteServer(config_path, server_log_path)
-    v1 = Vote(session_id="test_vote_1", candidate="1", voter_id="1122")
-    v2 = Vote(session_id="test_vote_1", candidate="1", voter_id="1323")
-    v3 = Vote(session_id="test_vote_1", candidate="2", voter_id="1122")
-    
-    _ = server.create_all([v1, v2, v3])
-    
+    vs = generate_votes() 
+    ids = server.create_all(vs)
+    print(server.record_ids)
     all_ = server.read_all()
     for a in all_:
         print(a)
--- src/datatype.py
+++ src/datatype.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
-from enum import Enum
-from hypothesis import given
-from hypothesis.strategies import SearchStrategy, composite, sampled_from, text
+from hypothesis.strategies import SearchStrategy, composite, sampled_from, text, lists
 from typing import Callable
+import string
 
 @dataclass
 class Vote:
@@ -22,5 +21,7 @@ def vote_gen(draw: Callable[[SearchStrategy], str]) -> Vote:
     
     session_id = draw(sampled_from(session_ids))
     candidate = draw(sampled_from(candidates))
-    voter_id = draw(text(min_size=1, max_size=10)) 
+    voter_id = draw(text(min_size=1, max_size=10, alphabet=string.ascii_letters)) 
     return Vote(session_id=session_id, candidate=candidate, voter_id=voter_id)
+
+vote_list_gen = lists(vote_gen(), min_size=1, max_size=10)
