e9749dafa3e67a974b2558e3e7148a8dd51e5932
Ethan He <yfhe@ucdavis.edu>
2024-12-06 07:55:48 +0000

    server

--- client/client.py
+++ client/client.py
@@ -2,15 +2,16 @@ import xmlrpc.client
 import fire
 from src.util import load_server_config
 
+
 def main(config_path: str = "config.yaml"):
     host, port = load_server_config(config_path).unwrap()
-    s = xmlrpc.client.ServerProxy('http://localhost:8000')
-    print(s.pow(2,3))  # Returns 2**3 = 8
-    print(s.add(2,3))  # Returns 5
-    print(s.mul(5,2))  # Returns 5*2 = 10
+    s = xmlrpc.client.ServerProxy("http://localhost:8000")
+    print(s.add(2, 3))  # Returns 5
+    print(s.mul(5, 2))  # Returns 5*2 = 10
 
     # Print list of available methods
     print(s.system.listMethods())
+
 
 if __name__ == "__main__":
     fire.Fire(main)
--- serve.py
+++ serve.py
@@ -2,37 +2,33 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 import fire
 from src.util import load_server_config
+import logging
 
+
+def add(x, y):
+    return x + y
+
+
+def mul(x, y):
+    return x * y
 
 
 def serve(config_path: str = "config.yaml"):
+    logging.basicConfig(level=logging.INFO)
 
     host, port = load_server_config(config_path).unwrap()
-    
-    # Create server
-    with SimpleXMLRPCServer((host, port),
-                            requestHandler=SimpleXMLRPCRequestHandler) as server:
+
+    logging.info(f"Starting server on {host}:{port}")
+    with SimpleXMLRPCServer(
+        (host, port), requestHandler=SimpleXMLRPCRequestHandler
+    ) as server:
         server.register_introspection_functions()
 
-        # Register pow() function; this will use the value of
-        # pow.__name__ as the name, which is just 'pow'.
-        server.register_function(pow)
+        server.register_function(add, "add")
+        server.register_function(mul, "mul")
 
-        # Register a function under a different name
-        def adder_function(x, y):
-            return x + y
-        server.register_function(adder_function, 'add')
-
-        # Register an instance; all the methods of the instance are
-        # published as XML-RPC methods (in this case, just 'mul').
-        class MyFuncs:
-            def mul(self, x, y):
-                return x * y
-
-        server.register_instance(MyFuncs())
-
-        # Run the server's main loop
         server.serve_forever()
-        
+
+
 if __name__ == "__main__":
     fire.Fire(serve)
